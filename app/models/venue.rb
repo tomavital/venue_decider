75c4dd031cdb1fecccb675f0213298b7a9a8739f
rails -v <c:sites>
2017-05-23 15:35:02 -0500

    sddsds
    asddsa

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,2 +1,6 @@
 class Venue < ApplicationRecord
+
+  validates :cuisine, :presence => true
+  validates :name, :presence => true, :uniqueness => { :scope => :address }
+  validates :average_price, :numericality => { :only_integer => true, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 5000}
 end
