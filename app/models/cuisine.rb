184484523aa68f8ac202a44319ab3871ccfdf333
rails -v <c:sites>
2017-05-24 14:10:29 -0500

    dsdsds
    xzxz

--- app/models/cuisine.rb
+++ app/models/cuisine.rb
@@ -1,2 +1,4 @@
 class Cuisine < ApplicationRecord
+  has_many :preferences
+  has_many :venues
 end
--- app/models/price.rb
+++ app/models/price.rb
@@ -1,2 +1,6 @@
 class Price < ApplicationRecord
+
+  has_many :users, :foreign_key => "max_price"
+  has_many :venues, :foreign_key => "average_price"
+  
 end
