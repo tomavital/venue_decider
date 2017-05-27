67075e7ab3029a4f51954fcea3d63cdc26cba8a5
rails -v <c:sites>
2017-05-27 15:50:25 -0500

    perfecto

--- app/controllers/myevents_controller.rb
+++ app/controllers/myevents_controller.rb
@@ -1,7 +1,7 @@
 class MyeventsController < ApplicationController
   def index
-    @my_events= Event.where({:creator_id => current_user.id})
-
+    # @my_events= Event.where({:creator_id => current_user.id})
+@my_events= Guest.where({:user_id => current_user.id})
     render("my_events/index.html.erb")
 end
 end
--- app/models/my_events.rb
+++ app/models/my_events.rb
@@ -1,8 +1,7 @@
 class My_event < ApplicationRecord
-has_many :users, :foreign_key => "creator_id"
+has_many :users, :foreign_key => "user_id"
   validates :creator_id, :presence => true
 
   has_many :guests
-  belongs_to :creator, :class_name => "User"
   belongs_to :venue , :foreign_key => "venue_id"
 end
