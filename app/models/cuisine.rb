59ab4d899c2eaef0c2ec2bb086dcb2ce8afb6e11
rails -v <c:sites>
2017-05-26 23:37:35 -0500

    sffs
    fsfs

--- app/models/cuisine.rb
+++ app/models/cuisine.rb
@@ -1,4 +1,5 @@
 class Cuisine < ApplicationRecord
-  has_many :preferences
+  has_many :users, :through => :preferences
+  has_many(:preferences, :class_name => "Preference", :foreign_key => "cuisine_id")
   has_many :venues
 end
--- app/models/preference.rb
+++ app/models/preference.rb
@@ -1,2 +1,4 @@
 class Preference < ApplicationRecord
+  belongs_to(:user, :class_name => "User", :foreign_key => "user_id")
+  belongs_to(:cuisine, :class_name => "Cuisine", :foreign_key => "cuisine_id")
 end
