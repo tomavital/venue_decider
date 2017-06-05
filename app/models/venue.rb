03a15b3ae435c68a831faf42d5c74ad40a2f133b
rails -v <c:sites>
2017-06-04 22:45:15 -0500

    dddc

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -5,6 +5,5 @@ class Venue < ApplicationRecord
 
 
   has_many :events, :foreign_key => "venue_id"
-  belongs_to(:cuisine, :class_name => "Cuisine", :foreign_key => "cuisine")
   belongs_to(:price, :class_name => "Price", :foreign_key => "average_price")
 end
